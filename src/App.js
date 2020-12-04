ef92eaa3f29ed3af755c6433cd9b104de1d0c6bb
mahadi-nsu <mahadi.hassan@northsouth.edu>
2020-12-04 17:39:26 +0600

    intial routing added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      Amazon Clone!!
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path = '/checkout'>
+            <h1>checkout</h1>
+          </Route>
+          <Route path = '/login'>
+            <h1>login</h1>
+          </Route>
+          <Route path = '/'>
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
